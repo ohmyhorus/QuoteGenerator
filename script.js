cd281d8551b5345846381fa99c4f381aa8c82a86
Sasha <45601549+ohmyhorus@users.noreply.github.com>
2023-04-26 19:33:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
 let apiQuotes = [];
 
 function newQuote() {
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  console.log(quote);
+  // check if author field is blank, replace it with 'unknown'
+  if (!quote.author) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+  // quote length check
+  if (quote.text.length > 40) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  quoteText.textContent = quote.text;
 }
 
 // quotes from api
@@ -16,6 +34,16 @@ async function getQuotes() {
     // error here
   }
 }
+
+//tweet quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+
+//event listener
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
 
 // on load
 getQuotes();
